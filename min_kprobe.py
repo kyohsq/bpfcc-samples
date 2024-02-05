4e6c554253c66f1ddbf3c95f008742eaa8e8bfcd
user <user@ubuntu22-playground.lan>
2024-02-05 02:34:11 +0000

    rename min_execve.py

--- min_kprobe.py
+++ min_kprobe.py
@@ -1,8 +1,10 @@
 from bcc import BPF
 
-BPF(text='''
+BPF(
+    text="""
 int kprobe____x64_sys_execve(void *ctx){
     bpf_trace_printk("hi");
     return 0;
 }
-''').trace_print()
+"""
+).trace_print()
